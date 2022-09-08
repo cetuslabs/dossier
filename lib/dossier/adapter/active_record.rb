e8e7cdf06736e3d1e4ba83c1251835894290be9a
James Moseley <james@kickserv.com>
2022-09-08 17:16:54 -0500

    working on fixing issue with disconnections

--- lib/dossier/adapter/active_record.rb
+++ lib/dossier/adapter/active_record.rb
@@ -2,20 +2,20 @@ module Dossier
   module Adapter
     class ActiveRecord
 
+
       attr_accessor :options, :connection
 
       def initialize(options = {})
         self.options    = options
-        self.connection = ::ActiveRecord::Base.connection || options.delete(:connection) || active_record_connection
       end
 
       def escape(value)
-        connection.quote(value)
+        active_record_connection.quote(value)
       end
 
       def execute(query, report_name = nil)
         # Ensure that SQL logs show name of report generating query
-        Result.new(connection.exec_query(*["\n#{query}", report_name].compact))
+        Result.new(active_record_connection.exec_query(*["\n#{query}", report_name].compact))
       rescue => e
         raise Dossier::ExecuteError.new "#{e.message}\n\n#{query}"
       end
@@ -23,7 +23,8 @@ module Dossier
       private
 
       def active_record_connection
-                @abstract_class = Class.new(::ActiveRecord::Base) do
+        return @abstract_class.connection if  @abstract_class&.connection.present? && @abstract_class&.connection.connected?
+        @abstract_class = Class.new(::ActiveRecord::Base) do
           self.abstract_class = true
           
           # Needs a unique name for ActiveRecord's connection pool
@@ -31,11 +32,9 @@ module Dossier
             "Dossier::Adapter::ActiveRecord::Connection_#{object_id}"
           end
         end
-        @abstract_class.establish_connection(options)
+        @abstract_class.establish_connection(options[:replica])
         @abstract_class.connection
       end
-
     end
-
   end
 end
