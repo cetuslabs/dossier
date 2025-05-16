67f2585877039802a51d2e3a8e203ab598dd81cd
Kamalraj Matheswaran <kamalraj.matheswaran@kaleris.com>
2025-05-16 16:48:05 +0530

    Add XLSX Logic

--- lib/dossier/xlsx.rb
+++ lib/dossier/xlsx.rb
@@ -6,19 +6,21 @@ module Dossier
     end
 
     def each
-      io = StringIO.new
-      workbook  = FastExcel.open(io, constant_memory: true)
-      worksheet = workbook.add_worksheet("Sheet1")
+      Tempfile.create(['dossier', '.xlsx']) do |tempfile|
+        workbook  = FastExcel.open(tempfile.path, constant_memory: true)
+        worksheet = workbook.add_worksheet("Sheet1")
 
-      worksheet.append_row(@headers)
-      @collection.each { |row| worksheet.append_row(row) }
+        worksheet.append_row(@headers)
+        @collection.each { |row| worksheet.append_row(row) }
 
-      workbook.close
-      io.rewind
+        workbook.close
 
-      # yield in binary chunks (e.g. 16KB) to support streaming
-      while (chunk = io.read(16 * 1024))
-        yield chunk
+        # Now stream the XLSX file in chunks
+        File.open(tempfile.path, 'rb') do |file|
+          while chunk = file.read(16 * 1024) # 16KB chunks
+            yield chunk
+          end
+        end
       end
     end
   end
